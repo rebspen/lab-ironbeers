29c05b430169c121f70a108d67b27190c87f87bc
Rebecca Spence <Bec@Rebeccas-MacBook-Pro.local>
2019-11-13 18:37:23 +0000

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,11 +9,43 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(__dirname+ "/views/partials")
 
 
 
 app.get('/', (req, res, next) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {  
+  res.render('beers', { beers });
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  
+});
+
+app.get('/randombeers', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(randombeers => {
+    res.render('randombeers', {randombeers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
+
+app.get('/:id', (req, res, next) => {
+  punkAPI.getBeer(req.params.id)
+  .then(singlebeer => { 
+  res.render('singlebeer' , {singlebeer});
+  })
+  .catch(error => {
+    console.log(error)
+  })
 });
 
 
